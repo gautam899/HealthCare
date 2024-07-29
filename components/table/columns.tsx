fb3947189aa26eb529e62489ae34b32dfebd7bea
Bhavya Gautam <124019261+gautam899@users.noreply.github.com>
2024-07-29 23:02:24 +0530

    Corrected the undefined error

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -52,13 +52,13 @@ export const columns: ColumnDef<Appointment>[] = [
             return (
                 <div className="flex items-center gap-3">
                     <Image
-                        src={doctor.image}
-                        alt={doctor.name}
+                        src={doctor?.image!}
+                        alt="doctor"
                         width={100}
                         height={100}
                         className="size-8"
                     />
-                    <p className="whitespace-nowrap">Dr. {doctor.name}</p>
+                    <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
                 </div>
             )
         },
